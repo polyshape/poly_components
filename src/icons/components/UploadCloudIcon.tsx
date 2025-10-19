34b8e9f0e0655874bdb6ade0beb656cd9c0ba631
Marios Papamarkou <mariosdrth@yahoo.gr>
2025-10-19 20:11:34 +0200

    Visual updates

--- src/icons/components/UploadCloudIcon.tsx
+++ src/icons/components/UploadCloudIcon.tsx
@@ -4,8 +4,8 @@ export function UploadCloudIcon() {
       <path
         d="M5.5 16.5H18a3.5 3.5 0 0 0 0-7 5.5 5.5 0 0 0-10.4-1.7A4 4 0 0 0 5.5 16.5Z"
       />
-      <path d="M12 12v7" stroke="currentColor" strokeLinecap="round" />
-      <path d="M9.5 14.5L12 12l2.5 2.5" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M12 8v6" stroke="currentColor" strokeLinecap="round" />
+      <path d="M9.5 10.5L12 8l2.5 2.5" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
     </>
   );
 }
